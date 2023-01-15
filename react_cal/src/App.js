c8e119b958fdb89ecc23cbd805cda995dfa8ed61
Taeyeon-dev <qazppo@naver.com>
2023-01-15 16:26:18 -0500

    Create Model.css, Modal.js, edit App.js

--- react_cal/src/App.js
+++ react_cal/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Main from './Main'
+import Upload from './Upload'
+import Calendar from './redux/modules/calendar'
+import NotFound from './NotFound'
+import {Route} from 'react-router-dom'
+import {withRouter} from 'react-router'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route>
+        <Route exact path="/" component={Main}/>
+        <Route exact path="/upload" component={Upload} />
+        <Route exact path="/calendar" component={Calendar} />      </Route>
     </div>
-  );
+  )
 }
 
-export default App;
+export default (withRouter(App));
